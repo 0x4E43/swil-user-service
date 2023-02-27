d0993decaf3a5dff4842461cb32a75a510590cd6
0x4E43 <talk2nimai@gmail.com>
2023-02-28 00:22:20 +0530

    javax changed to jakarta

--- src/main/java/com/swil/userservice/user/entity/UserDetailsBean.java
+++ src/main/java/com/swil/userservice/user/entity/UserDetailsBean.java
@@ -1,12 +1,14 @@
 package com.swil.userservice.user.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity(name="swil_user_details")
+@Entity
+@Table(name="swil_user_details")
 public class UserDetailsBean {
 
     @Id
