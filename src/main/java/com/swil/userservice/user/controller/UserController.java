1b0b41450fc40c575e8608c926c027bf3c44773d
0x4E43 <talk2nimai@gmail.com>
2023-02-28 01:07:27 +0530

    user controller for test added, user details service enhanceed

--- src/main/java/com/swil/userservice/user/controller/UserController.java
+++ src/main/java/com/swil/userservice/user/controller/UserController.java
@@ -1,5 +1,24 @@
 package com.swil.userservice.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.swil.userservice.user.dto.UserDetailsDto;
+import com.swil.userservice.user.service.UserDetailsService;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
     
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveUser(@RequestBody UserDetailsDto user){
+        return userDetailsService.saveUser(user);
+    }
+
 }
--- src/main/java/com/swil/userservice/user/service/UserDetailsService.java
+++ src/main/java/com/swil/userservice/user/service/UserDetailsService.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.swil.userservice.core.dto.Response;
+import com.swil.userservice.core.utils.UserLoginUtils;
 import com.swil.userservice.user.dao.UserDetailsDao;
 import com.swil.userservice.user.dto.UserDetailsDto;
 import com.swil.userservice.user.entity.UserDetailsBean;
@@ -17,16 +19,21 @@ public class UserDetailsService {
     @Autowired
     UserDetailsDao userDetailsDao;
 
-    public void saveUser(UserDetailsDto dto) {
+    public String saveUser(UserDetailsDto dto) {
         try {
             UserDetailsBean bean = new UserDetailsBean();
             bean.setEmailId("talk2nimai@gmail.com");
             bean.setUsername("nimai.c");
             bean.setPassword("Nimai@123");
             bean.setRole((short) 1);
-            userDetailsDao.save(bean);
+            bean.setUUID(UserLoginUtils.generateUuid());
+            bean = userDetailsDao.save(bean);
+            dto.setId(bean.getId());
+            dto.setUUID(bean.getUUID());
+            return Response.success("User added successfully", dto);
         } catch (Exception e) {
             logger.error("ERROR WHILE SAVING USER", e);
+            return Response.error("Unable to save user details");
         }
     }
 
