2a0fc978c9761f1f6e0e3589efbd7f01172678ca
0x4E43 <talk2nimai@gmail.com>
2023-02-28 23:42:30 +0530

    UserInfo Bean fields added

--- src/main/java/com/swil/userservice/user/entity/UserInfoBean.java
+++ src/main/java/com/swil/userservice/user/entity/UserInfoBean.java
@@ -7,32 +7,67 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.util.Date;
+
 @Entity
-@Table(name="swil_user_details")
+@Table(name = "swil_user_info")
 public class UserInfoBean {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
 
-    @Column(name="uuid")
+    @Column(name = "uuid")
     private String UUID;
 
-    @Column(name="username")
+    @Column(name = "username")
     private String username;
 
-    @Column(name="email_id")
+    @Column(name = "email_id")
     private String emailId;
 
-    @Column(name="mobile")
+    @Column(name = "mobile")
     private String mobile;
 
-    @Column(name="password")
+    @Column(name = "password")
     private String password;
 
-    @Column(name="role")
-    private Short role; //USER:3, MANAGER: 2, ADMIN: 1
+    @Column(name = "role")
+    private Short role; // USER:3, MANAGER: 2, ADMIN: 1
+
+    @Column(name = "f_name")
+    private String fname;
+
+    @Column(name = "l_name")
+    private String lname;
+
+    @Column(name = "profile_img")
+    private String profileImage;
+
+    @Column(name = "reset_key")
+    private String resetKey;
+
+    @Column(name = "is_deleted")
+    Boolean isDeleted;
+
+    @Column(name = "is_disabled")
+    Boolean isDisabled;
+
+    @Column(name = "last_login")
+    private Date lastLogin;
+
+    @Column(name = "created_by")
+    private Long createdBy;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "modified_by")
+    private Long modifiedBy;
+
+    @Column(name = "modified_date")
+    private Date modifiedDate;
 
     public int getId() {
         return id;
@@ -90,6 +125,92 @@ public class UserInfoBean {
         this.role = role;
     }
 
-    
-    
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public String getResetKey() {
+        return resetKey;
+    }
+
+    public void setResetKey(String resetKey) {
+        this.resetKey = resetKey;
+    }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setIsDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
+
+    public Boolean getIsDisabled() {
+        return isDisabled;
+    }
+
+    public void setIsDisabled(Boolean isDisabled) {
+        this.isDisabled = isDisabled;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Long modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
 }
