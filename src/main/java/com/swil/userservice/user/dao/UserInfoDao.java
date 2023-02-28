b4ec6f5f7193c2f65752ef20c78bcd72af167432
0x4E43 <talk2nimai@gmail.com>
2023-03-01 00:27:26 +0530

    findByEmailIdIgnoreCaseOrMobile method added to userInfo dao

--- src/main/java/com/swil/userservice/user/dao/UserInfoDao.java
+++ src/main/java/com/swil/userservice/user/dao/UserInfoDao.java
@@ -5,5 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.swil.userservice.user.entity.UserInfoBean;
 
 public interface UserInfoDao extends JpaRepository<UserInfoBean, Long>{
+
+    UserInfoBean findByEmailId(String emailId);
+
+    UserInfoBean findByEmailIdIgnoreCaseOrMobile(String emailId);
     
 }
